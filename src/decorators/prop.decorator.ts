e51b1b5acf1c10c0cb1ffa9dd1c604f30335e856
Can Çevik <mazlumcancevik@gmail.com>
2023-01-31 17:49:40 +0300

    add schema factory

--- src/decorators/prop.decorator.ts
+++ src/decorators/prop.decorator.ts
@@ -1,10 +1,10 @@
 import { PropOptions } from '../interfaces'
 import { TypeMetadataStorage } from '../storages'
 
-export function Prop(options: PropOptions = {}): MethodDecorator {
+export function Prop(options: PropOptions = {}): Function {
   return (target: object, propertyKey: string | symbol): void => {
     TypeMetadataStorage.addPropertyMetadata({
-      target,
+      target: target.constructor,
       propertyKey,
       options
     })
---
+++ src/factories/schema.factory.ts
@@ -0,0 +1,24 @@
+import mongoose, { Model, Schema } from 'mongoose'
+
+import { Type } from '../interfaces'
+import { TypeMetadataStorage } from '../storages'
+
+export class SchemaFactory {
+  public static createForClass<T>(name: string, target: Type<T>): Model<T> {
+    const schemaMetadata = TypeMetadataStorage.getSchemaMetadataByTarget(target)
+    const propertyMetadata = TypeMetadataStorage.getClassFieldsByPredicate(
+      (item) => item.target === target
+    )
+
+    const schema = new Schema(
+      Object.assign(
+        {},
+        ...propertyMetadata.map((item) => {
+          return { [item['propertyKey']]: item.options }
+        })
+      ),
+      schemaMetadata?.options
+    )
+    return mongoose.model<T>(name, schema)
+  }
+}
