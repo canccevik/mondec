fd0d7a5dd3a976b2a496651b3a38af613e772b2f
Can Çevik <mazlumcancevik@gmail.com>
2023-06-19 18:44:29 +0300

    refactor schema factory test

--- test/schema-factory.test.ts
+++ test/schema-factory.test.ts
@@ -4,11 +4,21 @@ import { SchemaFactory } from '../src'
 import { User } from './schemas'
 
 describe('Schema Factory', () => {
-  it('should create schema for class', () => {
-    const userSchema = SchemaFactory.createForClass(User)
+  let userSchema: Schema<User>
 
+  beforeAll(() => {
+    userSchema = SchemaFactory.createForClass(User)
+  })
+
+  it('should create schema for given class', () => {
     expect(userSchema instanceof Schema).toBe(true)
+  })
+
+  it('should schema apply given schema options', () => {
     expect((userSchema as any).options.versionKey).toBe(false)
+  })
+
+  it('should schema have defined properties', () => {
     expect(userSchema.obj.username).toStrictEqual({ type: String, unique: true, required: true })
     expect(userSchema.obj.age).toStrictEqual({ type: Number })
   })
